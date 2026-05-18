c73b1b9d174ac7f737f650aec192615fc8596003
Pank-cake <od.uvanchik@yandex.ru>
2026-05-18 11:41:25 +0500

    Program.cs

--- Lab_10/Program.cs
+++ Lab_10/Program.cs
@@ -5,7 +5,6 @@ class Program
 {
     static void Main()
     {
-        // Настройка кодировки для корректного вывода кириллицы в консоли Windows
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         var tests = new CompilerTests();
